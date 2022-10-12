7e677b6f2b127f45cb7e8d25c2cd8b681b75ff6c
datct <>
2022-10-12 17:27:46 +0700

    add 2 empty screens Login and Home

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => alert ('Hello!')} style={styles.button}>
+        <Text style={styles.buttonText}>Hello</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +19,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
